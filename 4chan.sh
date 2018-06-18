e073ab6159dc60a579abacc77483603bcaded53a
Ramu Thumu <ramuthumu33@gmail.com>
2018-06-18 00:26:15 -0500

    updated link search

--- 4chan.sh
+++ 4chan.sh
@@ -2,6 +2,6 @@
 rm -f link.txt
 dir=$(echo $1 | awk -F/ '{print $4"/"$7}')
 
-lynx --dump $1 | awk 'NR%2==0'| awk '/http/{print $2}' | grep 4cdn >> link.txt
+lynx --dump $1 | awk 'NR%2==0'| awk '/http/{print $2}' | grep jpg  >> link.txt
 
 aria2c -x 16 -j 5 -i link.txt -d "$dir" $i
